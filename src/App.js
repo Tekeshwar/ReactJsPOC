2c204db91ecb08f29a5e3672adfebae1b4131443
Tekeshwar <tekeshpixel@gmail.com>
2018-10-13 19:25:00 +0530

    added home component and completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import Header from './components/Header'; 
+import Header from './components/Header';
+import Home from './components/Home';
+
 import logo from './logo.svg';
 import './App.css';
- import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-class App extends React.Component  {
+class App extends React.Component {
   render() {
     return (
 
       <div className="App">
-        <Header /> 
+        <Header />
+        <Home />
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div id='home'>
+                <h4>Home Component</h4>
+            </div>
+        )
+    };
+} 
+export default Home; 
